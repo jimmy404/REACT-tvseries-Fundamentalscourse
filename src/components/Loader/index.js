fb13351b227e0a07c7be892b3b186cc10e29f83b
Jimmy <bruno_pier@hotmail.com>
2019-12-28 00:08:28 -0300

    Adding loading code

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import loaderSrc from '../../assets/loader.gif';
 
 const Loader = props => (
-    <div></div>
+    <div>
+        <img
+        style={{width: 40}}
+        alt="Loader icon"
+        src={loaderSrc}
+        />
+    </div>
 );
 
 export default Loader;

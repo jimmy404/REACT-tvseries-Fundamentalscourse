6b53c5751dd9fa5d0632c661e9333ae00d081e0c
Jimmy <bruno_pier@hotmail.com>
2019-12-27 21:46:28 -0300

    Adding new folder & file location

---
+++ src/components/App/index.js.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Intro from '../Intro';
+import './App.css';
+
+class App extends Component {
+  state ={
+    series: []
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV series List</h1>
+        </header>
+        <Intro message="Here you can find all of your most loved series" />
+        The lenght of series array - {this.state.series.length}
+      </div>
+    );
+  }
+}
+
+export default App;

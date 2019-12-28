f44dbc315eff76875edc19a8fba388f06ad53efb
Jimmy <bruno_pier@hotmail.com>
2019-12-27 22:27:05 -0300

    Adding Div message - state - componentDidMount in Series/index.js

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class Series extends Component {
+    state ={
+        series: []
+    }
+
+
+    componentDidMount(){
+        fetch('https://api.tvmaze.com/search/shows?q=Vikings')
+        .then(response => response.json())
+        .then(json => this.setState({series: json}))
+    }
     render(){
         return(
-            <div>Series Container</div>
+            <div>
+            The lenght of series array - {this.state.series.length}
+            </div>
         )
     }
 }

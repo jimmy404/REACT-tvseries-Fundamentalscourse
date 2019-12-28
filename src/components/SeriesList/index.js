082b08c2b71cec0ff0202a3aefdf713c1436d530
Jimmy <bruno_pier@hotmail.com>
2019-12-27 23:31:53 -0300

    Solving key error

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './index.css';
 
 const SeriesListItem = ({ series }) => (
-    <li key={series.show.id}>{series.show.name}</li>
+    <li>
+        {series.show.name}
+    </li>
 )
 
 const SeriesList = (props) => {
@@ -10,7 +12,7 @@ const SeriesList = (props) => {
         <div>
             <ul className="series-list">
                 {props.list.map(series => (
-                    <SeriesListItem series={series} />
+                    <SeriesListItem series={series} key={series.show.id} />
                 ))}
             </ul>
         </div>

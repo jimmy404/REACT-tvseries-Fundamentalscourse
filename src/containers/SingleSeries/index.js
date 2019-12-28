83e49a697f6185bf7f261cb93de7cbbdb0e7e54a
Jimmy <bruno_pier@hotmail.com>
2019-12-28 00:50:17 -0300

    Adding SingleSeries file&folder&code

---
+++ src/containers/SingleSeries/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleSeries extends Component {
+    render() {
+        return(
+            <div>
+                <p>single Series</p>
+            </div>
+        )
+    }
+}
+
+export default SingleSeries;

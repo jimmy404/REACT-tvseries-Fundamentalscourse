19f8a0e3cccbcab243d7a362fe31e9b5b58c9397
Jimmy <bruno_pier@hotmail.com>
2019-12-29 21:39:23 -0300

    Ordering code in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,14 +8,11 @@ class App extends Component {
     series: []
   }
 
-
   componentDidMount(){
     fetch('https://api.tvmaze.com/search/shows?q=Vikings')
     .then(response => response.json())
     .then(json => this.setState({series: json}))
   }
-
-
 
   render(){
     return (

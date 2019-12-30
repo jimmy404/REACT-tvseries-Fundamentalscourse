154796b80e027d764f73c959ab8425241b094f47
Jimmy <bruno_pier@hotmail.com>
2019-12-29 21:43:49 -0300

    Ordering imports in Main - index.js

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Series from '../../containers/Series';
 import SingleSeries from '../../containers/SingleSeries';
 
